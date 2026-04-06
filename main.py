653cf205bdde30663ff827f93127f459efcf3246
fr-a-nk <frankcorporaal@gmail.com>
2026-04-06 16:30:06 +0200

    Moving triangle groups

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)  
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Game Loop
     while True:
         log_state()
@@ -30,8 +36,10 @@ def main():
 
         dt = clock.tick(60) / 1000
 
-        player.draw(screen)
-        player.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        
+        updatable.update(dt)
 
 
         # End of Game Loop
