89ca7b0f1a57dd351ad38f3f877816ab51e52d2a
fr-a-nk <frankcorporaal@gmail.com>
2026-04-06 17:38:05 +0200

    showing bullets

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 from logger import log_event
 
 def main():
@@ -20,10 +21,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
 
     Player.containers = (updatable, drawable)  
     Asteroid.containers = (asteroids, updatable, drawable) 
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)  
 
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -43,13 +47,16 @@ def main():
 
  
 
-        dt = clock.tick(60) / 1000
 
-        for thing in drawable:
-            thing.draw(screen)
+
+
         
+        dt = clock.tick(60) / 1000
         updatable.update(dt)
         
+        for thing in drawable:
+            thing.draw(screen)
+
         # asteroids
         for ast in asteroids:
             if ast.collides_with(player):
--- player.py
+++ player.py
@@ -1,6 +1,8 @@
-from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED
 import pygame
+from circleshape import CircleShape
+from shot import Shot
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -40,3 +42,10 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        s = Shot(self.position.x, self.position.y)
+        s.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
